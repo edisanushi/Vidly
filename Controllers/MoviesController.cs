ab997683f11bb50efcdc2e826124d2cd1091c80a
edisanushi <edisanushi@gmail.com>
2023-10-07 20:06:59 +0200

    Action parameters

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -18,5 +18,27 @@ namespace Vidly.Controllers
             //return Content("Hello World");
             return View(movie);
         }
+
+
+
+        //Can get accessed both ways:
+        //1:   /Movies/Edit?id=1
+        //2:   /Movies/Edit/1
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
     }
 }
