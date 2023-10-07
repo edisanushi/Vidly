40158989700bea6626a633af76174e583b981d24
edisanushi <edisanushi@gmail.com>
2023-10-07 22:08:48 +0200

    updated with main again

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movies() { Id = 1, Title = "Shrek" };
+            //return RedirectToAction("Index", "Home", new {page=1, sortBy = "name"}); 
+            //return HttpNotFound();
+            //return Content("Hello World");
+            return View(movie);
+        }
+
+
+
+        //Can get accessed both ways:
+        //1:   /Movies/Edit?id=1
+        //2:   /Movies/Edit/1
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+
+
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
